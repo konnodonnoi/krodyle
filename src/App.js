58796091a5686c5406aef4fb5259d248f37a17c8
me <saboanamero8521@gmail.com>
2022-07-15 00:35:20 +0100

    commits

--- src/App.js
+++ src/App.js
@@ -9,12 +9,13 @@ export const AppContext = createContext();
 
 function App() {
   const [board, setBoard] = useState(boardDefault);
+  const [currAttempt, setCurrAttempt] = useState({attempt: 0, letterPos: 0})
   return (
     <div className="App">
       <nav>
         <img src="../images/raccoon1.png" alt="raccoon" className="logo" />
       </nav>
-      <AppContext.Provider value={{ board, setBoard }}>
+      <AppContext.Provider value={{ board, setBoard, currAttempt, setCurrAttempt }}>
         <div className="game">
       <Board />
       <Keyboard />
--- src/components/Key.js
+++ src/components/Key.js
@@ -1,15 +1,22 @@
-import React, {useContext} from "react";
-import {AppContext} from "../App";
-
+import React, { useContext } from "react";
+import { AppContext } from "../App";
 
 const Key = ({ keyval, largeKey }) => {
-  const {board, setBoard} = useContext(AppContext);
+  const { board, setBoard, currAttempt, setCurrAttempt } =
+    useContext(AppContext);
 
   const selectLetter = () => {
-    const newBox = [...board]
-    newBox[0][0] = keyval
-    setBoard(newBox)
-  }
+    if (keyval === "ENTER") {
+      if (currAttempt.letterPos !== 5) return;
+      setCurrAttempt({ attempt: currAttempt.attempt + 1, letterPos: 0 });
+    } else {
+      if (currAttempt.letterPos > 4) return;
+      const newBox = [...board];
+      newBox[currAttempt.attempt][currAttempt.letterPos] = keyval;
+      setBoard(newBox);
+      setCurrAttempt({ ...currAttempt, letterPos: currAttempt.letterPos + 1 });
+    }
+  };
   return (
     <div className="key" id={largeKey && "big"} onClick={selectLetter}>
       {keyval}
