fb09cf8d6960e96740e8507d9a7381e7ffca2dcb
me <saboanamero8521@gmail.com>
2022-07-15 00:17:53 +0100

    commits

--- src/components/Key.js
+++ src/components/Key.js
@@ -1,8 +1,17 @@
-import React from "react";
+import React, {useContext} from "react";
+import {AppContext} from "../App";
+
 
 const Key = ({ keyval, largeKey }) => {
+  const {board, setBoard} = useContext(AppContext);
+
+  const selectLetter = () => {
+    const newBox = [...board]
+    newBox[0][0] = keyval
+    setBoard(newBox)
+  }
   return (
-    <div className="key" id={largeKey && "big"}>
+    <div className="key" id={largeKey && "big"} onClick={selectLetter}>
       {keyval}
     </div>
   );
