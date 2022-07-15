1c064170a9f0ef2da5bd322cd3c2878375bab8d0
me <saboanamero8521@gmail.com>
2022-07-14 21:36:42 -1200

    commits

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
+
 import "./App.css";
 import Board from "./components/Board";
 import Keyboard from "./components/Keyboard";
+import { createContext, useState } from "react";
+import { boardDefault } from "./components/Words";
+
+export const AppContext = createContext();
 
 function App() {
+  const [board, setBoard] = useState(boardDefault);
   return (
     <div className="App">
       <nav>
         <img src="../images/raccoon1.png" alt="raccoon" className="logo" />
       </nav>
+      <AppContext.Provider value={{ board, setBoard }}>
       <Board />
       <Keyboard />
+      </AppContext.Provider>
     </div>
   );
 }
